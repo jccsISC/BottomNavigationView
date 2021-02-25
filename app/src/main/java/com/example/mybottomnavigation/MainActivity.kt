0ce9dc402cd7cf64c29c1610918b07bbe85bf3dc
Julio Cesar <silva.jc@hotmail.com>
2021-02-25 10:26:56 -0600

    Base bottomNavigationView con navigation components

---
+++ app/src/main/java/com/example/mybottomnavigation/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.mybottomnavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.mybottomnavigation.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var mBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        mBinding.apply {
+            val navController = findNavController(R.id.contentFragmentView)
+
+            bottomNavigation.setupWithNavController(navController)
+        }
+    }
+}
